4e9032711d21b5b73ef7f7edc39af8bf05fff732
zhanghl <zhanghl@china-coast.com>
2024-07-29 16:41:15 +0800

    测试接口

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,7 @@
 export default function handler(request, response) {
-    response.status(200).send(request)
+    var sql = "SET @max_sort = (SELECT MAX(sort) FROM t_target);INSERT INTO t_target (name, count, price, sort) VALUES ('fuck', 1, 100, @max_sort + 1);"
+    
+    db.query(sql, (error, result) => {
+        response.status(200).send(request)
+    })
 }
