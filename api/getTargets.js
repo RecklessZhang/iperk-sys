7ace37b73a8937acadd28e1f362f23acb08d0e62
zhanghl <zhanghl@china-coast.com>
2024-07-29 15:03:11 +0800

    新增两个接口

--- api/getTargets.js
+++ api/getTargets.js
@@ -1,7 +1,7 @@
 const db = require('../db/config')
 
   export default function handler(request, response) {
-    var sql = 'select * from t_config'
+    var sql = 'select * from t_target'
     db.query(sql, (error, result) => {
         response.status(200).send(result)
     })
