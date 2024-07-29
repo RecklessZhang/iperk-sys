35d80d6328d65bb95894a6664f564ad7ecb763d3
zhanghl <zhanghl@china-coast.com>
2024-07-29 15:14:42 +0800

    修改查询config的语句

--- api/getConfig.js
+++ api/getConfig.js
@@ -1,7 +1,7 @@
 const db = require('../db/config')
 
   export default function handler(request, response) {
-    var sql = 'select * from t_config'
+    var sql = "select DATE_FORMAT(start_date, '%Y-%m-%d') as start_date, average_remain, interval_hour from t_config"
     db.query(sql, (error, result) => {
         response.status(200).send(result)
     })
