8373751a4e43323b76d6af4f18b84f46157eb8f9
zhanghl <zhanghl@china-coast.com>
2024-07-29 16:54:00 +0800

    添加增加目标的后台接口

--- api/addTarget.js
+++ api/addTarget.js
@@ -4,7 +4,7 @@ const db = require('../db/config')
     const { name, price, count } = request.query
 
     var sql = "SET @max_sort = (SELECT MAX(sort) FROM t_target); insert into t_target(name, price, count, sort) values ( ?, ?, ?, @max_sort + 1)"
-    db.query(sql, [name, price, count], (error, result) => {
+    db.query(sql, [name, price * 1, count * 1], (error, result) => {
         if (error) {
             console.error(error); // 输出错误信息到控制台
             response.status(500).send(error);
